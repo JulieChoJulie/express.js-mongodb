4d3ae236a7f4a5b89cc3356b3ae21e6e5addf37c
JulieChoJulie <h32cho@gmail.com>
2019-03-05 17:30:11 -0500

    app.js added

---
+++ app.js
@@ -0,0 +1,21 @@
+// LOAD PACKAGE
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+// CONFIGURE APP TO USE bodyParser
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// CONFIGURE SERVER PORT
+var port = process.env.PORT || 8080;
+
+// CONFIGURE ROUTER
+var router = require('./routers')(app);
+
+
+// RUN SERVER
+var server = app.listen(port, function(){
+    console.log("Express server has started on port " + port)
+});
