535f01a28680cde19530f1b9d0de089ce0f37e0b
JulieChoJulie <h32cho@gmail.com>
2019-03-05 17:47:55 -0500

    arouter added

--- app.js
+++ app.js
@@ -15,6 +15,18 @@ var port = process.env.PORT || 8080;
 var router = require('./routers')(app);
 
 
+// CONFIGURE mongoose
+
+// CONNECT TO MONGODB SERVER
+var db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', function(){
+    // CONNECTED TO MONGODB SERVER
+    console.log("Connected to mongod server");
+});
+
+mongoose.connect('mongodb://localhost/mongodb_tutorial');
+
 // RUN SERVER
 var server = app.listen(port, function(){
     console.log("Express server has started on port " + port)
